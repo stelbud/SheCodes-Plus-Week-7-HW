339f0c91e39c7fdeef689a18017a65f5e77e22ae
stelbud <stellabudi@hotmail.com>
2024-03-22 11:47:41 +0700

    Delete description in HTML then inject actual description

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ function changeWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
