2e3c68e32a37a8118f85e6452d07b78010a705c4
stelbud <stellabudi@hotmail.com>
2024-03-22 14:11:55 +0700

    Inject actual icon

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function changeWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
@@ -15,6 +16,10 @@ function changeWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img
+src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 }
 
 function formatDate(date) {
