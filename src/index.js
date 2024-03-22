e70142ad4af8042ec67ddc079fbe2d7217d5b068
stelbud <stellabudi@hotmail.com>
2024-03-22 10:09:52 +0700

    Create id search-form-input and create function handleSearchSubmit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
