f108d64a66f9204ea6e0c36769ea8a8d7ecbf134
stelbud <stellabudi@hotmail.com>
2024-03-22 10:56:39 +0700

    Create function searchCity with apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "683ca4t0db1024efbc065a4o1b7796e4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
